9d96701e883721fb412e9b40469324c0aa999c48
k.taniuchi <barbeekonta1995@gmail.com>
2021-12-11 17:41:44 +0900

    bootstrap導入、先行後攻表示機能追加

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,25 +1,39 @@
 "use strict";
 
+const gameStartBtn = document.getElementById("game-start-btn");
+
 //プレイヤーの名前を取得する関数
-document.getElementById('players-name-add-btn').addEventListener('click', () => {
-    const playerANameAdd = document.getElementById('playerA-name')
-    const playerBNameAdd = document.getElementById('playerB-name')
+gameStartBtn.addEventListener('click', () => {
+    const playerANameAdd = document.getElementById('playerA-name');
+    const playerBNameAdd = document.getElementById('playerB-name');
     
     if (playerANameAdd.value === '') {
         alert('プレイヤーAの名前を入力してください。');
-        return
+        playerANameAdd.focus();
+        return;
 
     } else if (playerBNameAdd.value === '') {
         alert('プレイヤーBの名前を入力してください。');
-        return
+        playerBNameAdd.focus();
+        return;
     
     } else if (playerANameAdd.value === playerBNameAdd.value) {
         alert('プレイヤーAとプレイヤーBの名前は別にしてください。');
-        return
+        playerBNameAdd.focus();
+        return;
 
     } else {
-        document.getElementById("game-start-btn").style.display ='block';
         const playerAName = playerANameAdd.value;
         const playerBName = playerBNameAdd.value;
+        const players = [playerAName, playerBName];
+        document.getElementById('players-name-add-form').style.display = 'none';
+        document.getElementById('game-board').style.display = 'block';
+        decidePlayFirst(players);
     }
 });
+
+//先行・後攻を決めて、それを表示する関数
+const decidePlayFirst = (players) => {
+    const playFirstPlayer = players[Math.floor(Math.random() * players.length)];
+    alert(`先行は${playFirstPlayer}様です。` );
+};
